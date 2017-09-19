c0111b06e6baa0a1e09a17881a093307e677afa2
Guest User <Guest@epicodus.local>
2017-09-19 08:05:11 -0700

    include proper display

--- js/solar-interface.js
+++ js/solar-interface.js
@@ -4,12 +4,13 @@ $(document).ready(function() {
   $('#solar-form').submit(function(event) {
     event.preventDefault();
     let bDay = $("#bDay").val();
-    let solarCalculator = new SolarCalc(bDay);
-    let age = $('#givenAge');
+    let solarCalculator = new SolarCalc("bDay");
+    let age = $('#givenAge').val();
     let merAge = solarCalculator.getMercury(age);
     let venAge = solarCalculator.getVenus(age);
     let marAge = solarCalculator.getMars(age);
     let jupAge = solarCalculator.getJupiter(age);
+    let world = "mercury";
     let expectedYears = solarCalculator.timeLeft(world,age);
 
       $('#solution').append("<li>" + "Your Earth age is:" + age);
@@ -17,6 +18,7 @@ $(document).ready(function() {
       $('#solution').append("<li>" + "Your Venus age is:" + venAge);
       $('#solution').append("<li>" + "Your Mars age is:" + marAge);
       $('#solution').append("<li>" + "Your Jupiter age is:" + jupAge);
-      $('#solution').append("<li>" + expectedYears);
+      $('#solution').append("<li>" + "Your Life expectancy is:" + expectedYears);
+
     });
   });
